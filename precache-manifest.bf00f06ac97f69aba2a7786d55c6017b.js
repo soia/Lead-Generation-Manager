b8ac8a2899e89084ace4d7b766ca290b68b61a3f
Nikita Soia <nikita.soya@gmail.com>
2020-08-07 17:23:25 +0300

    Updates

---
+++ precache-manifest.bf00f06ac97f69aba2a7786d55c6017b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f061ceb4dbff1939aad453f7065aeefb",
+    "url": "/soia/Lead-Generation-Manager/index.html"
+  },
+  {
+    "revision": "155cfb272bd846f97b70",
+    "url": "/soia/Lead-Generation-Manager/static/css/2.0f955e6a.chunk.css"
+  },
+  {
+    "revision": "c05b97e4bfac09f24cfc",
+    "url": "/soia/Lead-Generation-Manager/static/css/main.7907843a.chunk.css"
+  },
+  {
+    "revision": "155cfb272bd846f97b70",
+    "url": "/soia/Lead-Generation-Manager/static/js/2.24fe0e5a.chunk.js"
+  },
+  {
+    "revision": "c05b97e4bfac09f24cfc",
+    "url": "/soia/Lead-Generation-Manager/static/js/main.d0c14ec4.chunk.js"
+  },
+  {
+    "revision": "e066bcf47cc21e88d46a",
+    "url": "/soia/Lead-Generation-Manager/static/js/runtime~main.194a548e.js"
+  },
+  {
+    "revision": "09eeddc0cd5c2a6021c181fc25233b36",
+    "url": "/soia/Lead-Generation-Manager/static/media/404.09eeddc0.svg"
+  },
+  {
+    "revision": "a26c9782ee62b44ec0dbea5d0b8b426b",
+    "url": "/soia/Lead-Generation-Manager/static/media/Cera Pro Black Italic.a26c9782.otf"
+  },
+  {
+    "revision": "95d55d992d7f63799ad116dbd12a25cf",
+    "url": "/soia/Lead-Generation-Manager/static/media/Cera Pro Black.95d55d99.otf"
+  },
+  {
+    "revision": "07ac815392d30140366e0a9df7f769e8",
+    "url": "/soia/Lead-Generation-Manager/static/media/Cera Pro Bold.07ac8153.otf"
+  },
+  {
+    "revision": "112299d7917b38aff426d451d495f3d7",
+    "url": "/soia/Lead-Generation-Manager/static/media/Cera Pro Light.112299d7.otf"
+  },
+  {
+    "revision": "afe6902a46c9d424e9aca916f97562db",
+    "url": "/soia/Lead-Generation-Manager/static/media/Cera Pro Medium.afe6902a.otf"
+  },
+  {
+    "revision": "0b7b37be489a128959fe71c97850e08f",
+    "url": "/soia/Lead-Generation-Manager/static/media/Cera Pro Regular Italic.0b7b37be.otf"
+  },
+  {
+    "revision": "fdcbccd5b11bc5da07a775620df0842a",
+    "url": "/soia/Lead-Generation-Manager/static/media/Charles McCormick.fdcbccd5.svg"
+  },
+  {
+    "revision": "bd9d23cfd0b60391bdec2f2fa665a4b1",
+    "url": "/soia/Lead-Generation-Manager/static/media/David Summers.bd9d23cf.svg"
+  },
+  {
+    "revision": "5abf992a615066704e7fa8c47b12aca4",
+    "url": "/soia/Lead-Generation-Manager/static/media/Elizabeth Nelson.5abf992a.svg"
+  },
+  {
+    "revision": "83076a78ad9a9e1a276e8d4dd80a383d",
+    "url": "/soia/Lead-Generation-Manager/static/media/John Fleming.83076a78.svg"
+  },
+  {
+    "revision": "d8025ad184765a1d9601625dd3d818da",
+    "url": "/soia/Lead-Generation-Manager/static/media/Oval.d8025ad1.svg"
+  },
+  {
+    "revision": "5fb091b6b5710ce111e327aa90e24d10",
+    "url": "/soia/Lead-Generation-Manager/static/media/Robert Sutton.5fb091b6.svg"
+  },
+  {
+    "revision": "0ad7c063fdf281ce2115a963a24db7d0",
+    "url": "/soia/Lead-Generation-Manager/static/media/background.0ad7c063.svg"
+  },
+  {
+    "revision": "e04474ee25805e9eb74a6b86d4f8cc07",
+    "url": "/soia/Lead-Generation-Manager/static/media/background.e04474ee.svg"
+  },
+  {
+    "revision": "21a84586e3164e2cb6ccb0a9493bf53f",
+    "url": "/soia/Lead-Generation-Manager/static/media/error.21a84586.jpg"
+  },
+  {
+    "revision": "bc53ee568c46e2e16450cb825389390b",
+    "url": "/soia/Lead-Generation-Manager/static/media/facebook.bc53ee56.svg"
+  },
+  {
+    "revision": "3e89403362f8cc0557344ed066d31237",
+    "url": "/soia/Lead-Generation-Manager/static/media/firstScreenBg.3e894033.svg"
+  },
+  {
+    "revision": "b70fdb05c95e8bd66ed09c657168a247",
+    "url": "/soia/Lead-Generation-Manager/static/media/logoWithBlackLabel.b70fdb05.svg"
+  },
+  {
+    "revision": "9276981a75eaad8652dd9f13e27745a6",
+    "url": "/soia/Lead-Generation-Manager/static/media/logoWithWhiteLabel.9276981a.svg"
+  },
+  {
+    "revision": "7de182f1855690830451e5f057740594",
+    "url": "/soia/Lead-Generation-Manager/static/media/rectangle.7de182f1.svg"
+  },
+  {
+    "revision": "9c42fb227b15f91c1a5f7f4cab80a5bd",
+    "url": "/soia/Lead-Generation-Manager/static/media/roadmap.9c42fb22.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/soia/Lead-Generation-Manager/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/soia/Lead-Generation-Manager/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/soia/Lead-Generation-Manager/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/soia/Lead-Generation-Manager/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "858830ed701b9942e27a4cf26fbed9f1",
+    "url": "/soia/Lead-Generation-Manager/static/media/spinner.858830ed.svg"
+  },
+  {
+    "revision": "89589c3cf7ee0582ffe75f7c50ef3868",
+    "url": "/soia/Lead-Generation-Manager/static/media/telegram.89589c3c.svg"
+  },
+  {
+    "revision": "7637d9f2ad1fa9fc1418e4cd829e3e07",
+    "url": "/soia/Lead-Generation-Manager/static/media/twitter.7637d9f2.svg"
+  },
+  {
+    "revision": "7845d45279e58ef62e48cfc46797a8f6",
+    "url": "/soia/Lead-Generation-Manager/static/media/vk.7845d452.svg"
+  }
+]);
